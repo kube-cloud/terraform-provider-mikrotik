2f05b17eb15e92e147c141f94eb276f41c93898e
ETUNE NGI Jean-Jacques <jetune@kube-cloud.com>
2023-03-11 19:49:05 +0100

    Update resource.tf

--- examples/resources/mikrotik_bridge_interface/resource.tf
+++ examples/resources/mikrotik_bridge_interface/resource.tf
@@ -3,6 +3,6 @@ resource "mikrotik_bridge_interface" "bridge_resurce_name" {
   name ="bridge_name"
   disabled = false
   auto_mac = true
-  admin_mac = false
+  admin_mac = "74:4D:28:F3:A7:14"
   comment = "bridge description"
 }
